251f040c51418d2405340ebe67e9bbb6d1232e1a
Florian Goerke <florian@goerke.de>
2024-03-17 16:56:08 +0100

    add blog file check

--- src/app/[locale]/blog/[slug]/page.tsx
+++ src/app/[locale]/blog/[slug]/page.tsx
@@ -4,10 +4,15 @@ import { getLocale } from 'next-intl/server'
 import { MDXRemote } from 'next-mdx-remote/rsc'
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
+import { notFound } from 'next/navigation';
 const katex: any = rehypeKatex;
 
 export default async function Page({ params }: { params: { slug: string } }) {
-    const source = fs.readFileSync(path.resolve(process.cwd(), "blog-posts", `${await getLocale()}/${params.slug}.mdx`))
+    const blog_name = params.slug.replace(/[^0-9a-z\-]/gi, "");
+    const blog_path = path.resolve(process.cwd(), "blog-posts", `${await getLocale()}/${blog_name}.mdx`);
+
+    if (!fs.existsSync(blog_path)) notFound();
+    const source = await fs.readFileSync(blog_path)
     return (
         <div>
             <MDXRemote source={source} options={{ mdxOptions: { remarkPlugins: [remarkMath], rehypePlugins: [katex], format: 'mdx' } }} />
